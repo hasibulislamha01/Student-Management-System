175f118204f7defd1ad3e8a7500e88d532b2c989
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-07-06 22:36:35 +0600

    repo edited

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,13 +1,8 @@
-import Banner from "./Banner";
-import Skills from "./Skills";
-import WelcomeSection from "./WelcomeSection";
 
 const Home = () => {
     return (
-        <div className="space-y-32">
-            <Banner></Banner>
-            <WelcomeSection></WelcomeSection>
-            <Skills></Skills>
+        <div className="text-5xl text-center pt-12">
+            This is Homepage
         </div>
     );
 };
